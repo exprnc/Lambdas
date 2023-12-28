be770e7d064a604a3bf30cc5133a0b63948de653
Alfir Rakhimov <exprnc7@gmail.com>
2023-12-28 21:47:00 +0500

    updated Lambdas

---
+++ src/Main.kt
@@ -0,0 +1,19 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+    var addFive = { x: Int -> x + 5 }
+    println("Pass 6 to addFive: ${addFive(6)}")
+
+    var addInts = { x: Int, y: Int -> x + y }
+    val result = addInts.invoke(6,7)
+    println("Pass 6, 7 to addInts: $result")
+
+    val intLambda: (Int, Int) -> Int = { x, y -> x * y }
+    println("Pass 10, 11 to intLambda: ${intLambda(10, 11)}")
+
+    val addSeven: (Int) -> Int = { it + 7}
+    println("Pass 12 to addSeven: ${addSeven(12)}")
+
+    val myLambda: () -> Unit = { println("Hi!") }
+    myLambda()
+}
